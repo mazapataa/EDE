b6e1e76a9a5f3f8ee1a6090843498df51a5d4473
Miguel A. Zapata <m.alfonsozapata@gmail.com>
2026-07-03 15:44:12 -0600

    CPL no neutrino

--- cobaya_ultranest/modules/CPL_runs/DESIDR2+PP/cpl_config.py
+++ cobaya_ultranest/modules/CPL_runs/DESIDR2+PP/cpl_config.py
@@ -13,7 +13,7 @@ SAMPLING_PARAMS = {
     'H0':        {'label': r'H_0',                        'range': (60.0, 80.0)},
     'omega_b':   {'label': r'\Omega_\mathrm{b} h^2',      'range': (0.022, 0.0226)},
     'omega_cdm': {'label': r'\Omega_\mathrm{c} h^2',      'range': (0.11, 0.13)},
-    'm_ncdm':    {'label': r'm_\nu',                      'range': (0.0, 0.1)},
+#    'm_ncdm':    {'label': r'm_\nu',                      'range': (0.0, 0.1)},
     'w0_fld':    {'label': r'w_{0,\mathrm{DE}}',          'range': (-2.0, -0.5)},
     'wa_fld':    {'label': r'w_{a,\mathrm{DE}}',      'range': (-2.0, 2.0)},
     'tau_reio':  {'label': r'\tau_\mathrm{reio}',         'range': (0.01, 0.08)}
@@ -34,9 +34,12 @@ COBAYA_INFO = {
             "extra_args": {
                 "fluid_equation_of_state": "CLP",
                 "use_ppf": True,
+                #"N_ncdm": 1,
+                #"N_ur": 2.0328,
+                #"deg_ncdm": 3.0,
                 "N_ncdm": 1,
                 "N_ur": 2.0328,
-                "deg_ncdm": 3.0,
+                "m_ncdm": 0.06, 
                 "T_ncdm": 0.71611,
                 "tol_initial_Omega_r": 0.1
             }
@@ -54,8 +57,8 @@ COBAYA_INFO = {
         "H0":        {"prior": {"min": 20, "max": 100}, "latex": "H_0"},
         "omega_b":   {"prior": {"min": 0.005, "max": 0.1}, "latex": r"\Omega_\mathrm{b} h^2"},
         "omega_cdm": {"prior": {"min": 0.001, "max": 0.99}, "latex": r"\Omega_\mathrm{c} h^2"},
-        "m_ncdm":    {"prior": {"min": 0, "max": 1.667}, "latex": r"m_\nu"},
-        "w0_fld":    {"prior": {"min": -3, "max": 1}, "latex": r"w_{0,\mathrm{DE}}"},
+        #"m_ncdm":    {"prior": {"min": 0, "max": 1.667}, "latex": r"m_\nu"},
+        "w0_fld":    {"prior": {"min": -2.5, "max": -0.5}, "latex": r"w_{0,\mathrm{DE}}"},
         "wa_fld":    {"prior": {"min": -2.0, "max": 2.0}, "latex": r"\w_{a,\mathrm{DE}}"},
         "tau_reio":  {"prior": {"min": 0.01, "max": 0.8}, "latex": r"\tau_\mathrm{reio}"},
         
